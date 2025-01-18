2e980ac343d3a224d53be260da192ba152b3e681
ParisaAbdollahi <parisaabdollahi7@gmail.com>
2025-01-18 12:21:47 +0330

    init project

---
+++ KafkaSetupProject/src/main/java/org/microservice/kafkasetupproject/KafkaSetupProjectApplication.java
@@ -0,0 +1,13 @@
+package org.microservice.kafkasetupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSetupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSetupProjectApplication.class, args);
+    }
+
+}
---
+++ KafkaSetupProject/src/test/java/org/microservice/kafkasetupproject/KafkaSetupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microservice.kafkasetupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSetupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
